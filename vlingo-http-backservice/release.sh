4db004c14baed6317af5cc08b8e73458a3723eea
Oscar <galindero@gmail.com>
2018-09-04 23:43:49 +0200

    add [un]mapping routes + policies

--- vlingo-http-backservice/release.sh
+++ vlingo-http-backservice/release.sh
@@ -3,3 +3,6 @@
 docker build -t kaseoga/vlingo-examples-backservice .
 docker push kaseoga/vlingo-examples-backservice
 cf push vlingo-examples-backservice --docker-image kaseoga/vlingo-examples-backservice
+cf map-route vlingo-examples-backservice apps.internal --hostname vlingo-examples-backservice
+cf add-network-policy vlingo-examples-frontservice --destination-app vlingo-examples-backservice --port 8082 --protocol tcp
+cf delete-route cfapps.io --hostname vlingo-examples-backservice
