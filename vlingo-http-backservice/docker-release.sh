479c13899b4d6710b913a2a57b914713acbc8339
Oscar <galindero@gmail.com>
2018-09-07 23:30:29 +0200

    - DEPLOY.md in frontservice - manifest.yml for each service - bash script to docker-release

---
+++ vlingo-http-backservice/docker-release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t kaseoga/vlingo-examples-backservice .
+docker push kaseoga/vlingo-examples-backservice
--- vlingo-http-backservice/release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t kaseoga/vlingo-examples-backservice .
-docker push kaseoga/vlingo-examples-backservice
-cf push vlingo-examples-backservice --docker-image kaseoga/vlingo-examples-backservice
-cf map-route vlingo-examples-backservice apps.internal --hostname vlingo-examples-backservice
-cf add-network-policy vlingo-examples-frontservice --destination-app vlingo-examples-backservice --port 8082 --protocol tcp
-cf delete-route cfapps.io --hostname vlingo-examples-backservice
