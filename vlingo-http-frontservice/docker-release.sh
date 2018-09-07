6bdff1d43d7f5992d1359e81dc21306f2a57b372
Oscar <galindero@gmail.com>
2018-09-07 23:30:29 +0200

    - DEPLOY.md in frontservice - manifest.yml for each service - bash script to docker-release

--- vlingo-http-frontservice/docker-release.sh
+++ vlingo-http-frontservice/docker-release.sh
@@ -2,4 +2,3 @@
 
 docker build -t kaseoga/vlingo-examples-frontservice .
 docker push kaseoga/vlingo-examples-frontservice
-cf push vlingo-examples-frontservice --docker-image kaseoga/vlingo-examples-frontservice
