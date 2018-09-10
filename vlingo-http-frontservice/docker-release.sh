66bb08df48ddd40211bce13971f48a2dda0a5eb3
Oscar <galindero@gmail.com>
2018-09-10 16:39:44 +0200

    - multistage build for dockers - changes in DEPLOY.md - parametrized docker-release script

--- vlingo-http-frontservice/docker-release.sh
+++ vlingo-http-frontservice/docker-release.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-IMAGE=kaseoga/vlingo-examples-frontservice
+IMAGE=$1
+
+if [[ -z $IMAGE ]]; then
+    echo "Image name should be defined: ./docker-release.sh <image_name>"
+    exit 1;
+fi
 
 docker build -t $IMAGE .
 docker push $IMAGE
