f51477ff57020dca381c98fbe5343f2b805a75a8
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-11-02 08:16:12 +0100

    Clean up example to implement some of those best practices

--- vlingo-eventjournal/src/main/java/io/vlingo/eventjournal/counter/CounterActor.java
+++ vlingo-eventjournal/src/main/java/io/vlingo/eventjournal/counter/CounterActor.java
@@ -1,13 +1,17 @@
 package io.vlingo.eventjournal.counter;
 
 import io.vlingo.actors.Actor;
-import io.vlingo.eventjournal.counter.events.CounterDecreasedEvent;
-import io.vlingo.eventjournal.counter.events.CounterIncreasedEvent;
+import io.vlingo.eventjournal.counter.events.CounterDecreased;
+import io.vlingo.eventjournal.counter.events.CounterDecreasedAdapter;
+import io.vlingo.eventjournal.counter.events.CounterIncreased;
+import io.vlingo.eventjournal.counter.events.CounterIncreasedAdapter;
 import io.vlingo.symbio.store.eventjournal.EventJournal;
 
 public class CounterActor extends Actor implements Counter {
     private final String counterName;
     private final EventJournal<String> journal;
+    private final CounterIncreasedAdapter counterIncreasedAdapter;
+    private final CounterDecreasedAdapter counterDecreasedAdapter;
     private int currentCount;
     private int version;
 
@@ -16,17 +20,19 @@ public class CounterActor extends Actor implements Counter {
         this.journal = journal;
         this.currentCount = 0;
         this.version = 1;
+        this.counterIncreasedAdapter = new CounterIncreasedAdapter();
+        this.counterDecreasedAdapter = new CounterDecreasedAdapter();
     }
 
     @Override
     public void increase() {
         currentCount++;
-        journal.append(counterName, version++, new CounterIncreasedEvent(currentCount).toTextEvent());
+        journal.append(counterName, version++, counterIncreasedAdapter.serialize(new CounterIncreased(currentCount)));
     }
 
     @Override
     public void decrease() {
         currentCount--;
-        journal.append(counterName, version++, new CounterDecreasedEvent(currentCount).toTextEvent());
+        journal.append(counterName, version++, counterDecreasedAdapter.serialize(new CounterDecreased(currentCount)));
     }
 }
