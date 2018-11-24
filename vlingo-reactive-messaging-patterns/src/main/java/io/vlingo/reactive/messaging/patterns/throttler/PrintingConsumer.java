a349d832cf263e8366dd79ce02bc7a35ec3208e6
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-11-25 00:56:43 +0100

    Test for checking throttling

--- vlingo-reactive-messaging-patterns/src/main/java/io/vlingo/reactive/messaging/patterns/throttler/PrintingConsumer.java
+++ vlingo-reactive-messaging-patterns/src/main/java/io/vlingo/reactive/messaging/patterns/throttler/PrintingConsumer.java
@@ -1,43 +1,21 @@
 package io.vlingo.reactive.messaging.patterns.throttler;
 
 import io.vlingo.actors.Actor;
-import io.vlingo.actors.Definition;
-import io.vlingo.actors.World;
-
-import static io.vlingo.actors.Definition.NoParameters;
-import static io.vlingo.actors.Definition.has;
-import static io.vlingo.actors.Definition.parameters;
+import io.vlingo.actors.testkit.TestUntil;
 
 public class PrintingConsumer extends Actor implements Consumer {
     private final long startedAt;
+    private final TestUntil until;
 
-    public PrintingConsumer() {
+    public PrintingConsumer(TestUntil until) {
+        this.until = until;
         this.startedAt = System.currentTimeMillis();
     }
 
     @Override
     public void onReceiveMessage(String message) {
         final long messageTimestamp = System.currentTimeMillis();
-        logger().log(String.format("%d\t\t%s", messageTimestamp, message));
-    }
-
-    public static void main(String[] args) {
-        World world = World.startWithDefaults("lol");
-
-        Producer delegate = world.actorFor(has(RandomStringProducer.class, NoParameters), Producer.class);
-        Producer throttled = world.actorFor(has(ThrottledProducer.class, parameters(5, 1000, delegate)), Producer.class);
-        Consumer consumer = world.actorFor(has(PrintingConsumer.class, NoParameters), Consumer.class);
-
-        for (int i = 0; i < 10; i++) {
-            throttled.produceMessage(consumer);
-        }
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        world.terminate();
+        logger().log(String.format("%d\t\t%s", messageTimestamp - startedAt, message));
+        until.happened();
     }
 }
