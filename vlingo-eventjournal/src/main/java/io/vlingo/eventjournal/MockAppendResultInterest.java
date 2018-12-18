47425e94da5ef0c12d5cf859a59d7bc351435133
Vaughn Vernon <vaughn@forcomprehension.com>
2018-12-18 16:12:12 -0700

    Refactored to 0.7.5.

---
+++ vlingo-eventjournal/src/main/java/io/vlingo/eventjournal/MockAppendResultInterest.java
@@ -0,0 +1,33 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.eventjournal;
+
+import java.util.List;
+import java.util.Optional;
+
+import io.vlingo.common.Outcome;
+import io.vlingo.symbio.Source;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.Result;
+import io.vlingo.symbio.store.StorageException;
+import io.vlingo.symbio.store.journal.Journal.AppendResultInterest;
+
+public class MockAppendResultInterest implements AppendResultInterest<String> {
+
+  @Override
+  public <S> void appendResultedIn(Outcome<StorageException, Result> outcome, String streamName, int streamVersion,
+          Source<S> source, Optional<State<String>> snapshot, Object object) {
+    
+  }
+
+  @Override
+  public <S> void appendAllResultedIn(Outcome<StorageException, Result> outcome, String streamName, int streamVersion,
+          List<Source<S>> sources, Optional<State<String>> snapshot, Object object) {
+    
+  }
+}
