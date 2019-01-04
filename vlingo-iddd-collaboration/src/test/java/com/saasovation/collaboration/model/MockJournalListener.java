db4da344e872d6262e863d10939fdd04aa1765cc
Vaughn Vernon <vaughn@forcomprehension.com>
2019-01-03 17:20:16 -0700

    Support reduced generics and protocols.

--- vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/MockJournalListener.java
+++ vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/MockJournalListener.java
@@ -9,53 +9,56 @@ package com.saasovation.collaboration.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import io.vlingo.actors.testkit.TestUntil;
 import io.vlingo.symbio.Entry;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.journal.JournalListener;
 
-public class InMemoryJournalListener implements JournalListener<String> {
-  public final List<Entry<String>> allEntries = new CopyOnWriteArrayList<>();
-  public final List<State<String>> allSnapshots = new CopyOnWriteArrayList<>();
-  public TestUntil until;
+public class MockJournalListener implements JournalListener<String> {
+  public final List<Entry<String>> allEntries = new ArrayList<>();
+  public final List<State<String>> allSnapshots = new ArrayList<>();
+  public TestUntil until = TestUntil.happenings(0);
 
   @Override
   public void appended(final Entry<String> entry) {
+    synchronized (allEntries) {
     allEntries.add(entry);
-    allSnapshots.add(null);
+    allSnapshots.add(State.TextState.Null);
     until.happened();
+    }
   }
 
   @Override
   public void appendedWith(final Entry<String> entry, final State<String> snapshot) {
+    synchronized (allEntries) {
     allEntries.add(entry);
     allSnapshots.add(snapshot);
     until.happened();
+    }
   }
 
   @Override
   public void appendedAll(final List<Entry<String>> entries) {
+    synchronized (allEntries) {
     allEntries.addAll(entries);
-    final List<State<String>> snapshots = new ArrayList<>(entries.size());
     for (int idx = 0; idx < entries.size(); ++idx) {
-      snapshots.add(null);
+      allSnapshots.add(State.TextState.Null);
     }
-    allSnapshots.addAll(snapshots);
     until.happened();
+    }
   }
 
   @Override
-  public void appendedAllWith(final List<Entry<String>> entries, final State<String> snapshot) {
+  public synchronized void appendedAllWith(final List<Entry<String>> entries, final State<String> snapshot) {
+    synchronized (allEntries) {
     allEntries.addAll(entries);
-    final List<State<String>> snapshots = new ArrayList<>(entries.size());
     for (int idx = 0; idx < (entries.size() - 1); ++idx) {
-      snapshots.add(null);
+      allSnapshots.add(State.TextState.Null);
     }
-    snapshots.add(snapshot);
-    allSnapshots.addAll(snapshots);
+    allSnapshots.add(snapshot == null ? State.TextState.Null : snapshot);
     until.happened();
+    }
   }
 
   public int confirmExpectedEntries(final int count, final int retries) {
