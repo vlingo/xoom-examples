9320b32ba0b4bea20df2f330fc7311b37db70333
Brian Wehrle <bwolesa@gmail.com>
2019-01-29 11:46:05 +0100

    Remove test that is not ready

--- vlingo-ecommerce/src/test/java/io/vlingo/examples/ecommerce/OrderResourceShould.java
+++ vlingo-ecommerce/src/test/java/io/vlingo/examples/ecommerce/OrderResourceShould.java
@@ -140,10 +140,9 @@ public class OrderResourceShould {
         return orderUrl.split("/")[2];
     }
 
-    @Test
-    @Ignore
+    //@Test --Not implemented in the actor yet, causes a failure
+    //   
     public void orderIsPaid_whenPaymentReceived() throws IOException {
-        // Not implemented in the actor yet, causes a failure
         String orderUrl = createOrder();
         String orderId  = getOrderId(orderUrl);
 
