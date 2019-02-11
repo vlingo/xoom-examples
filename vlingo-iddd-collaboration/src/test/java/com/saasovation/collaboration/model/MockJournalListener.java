e7ce57791706df855215bef0a2292e20942d77d8
VaughnVernon <vaughn@forcomprehension.com>
2019-02-11 16:54:42 -0700

    Reimplemented with AccessSafely.

--- vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/MockJournalListener.java
+++ vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/MockJournalListener.java
@@ -7,81 +7,89 @@
 
 package com.saasovation.collaboration.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicReference;
 
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.common.Tuple2;
 import io.vlingo.symbio.Entry;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.journal.JournalListener;
 
 public class MockJournalListener implements JournalListener<String> {
-  private final List<Entry<String>> allEntries = new CopyOnWriteArrayList<>();
-  private final List<State<String>> allSnapshots = new CopyOnWriteArrayList<>();
-  private final AtomicReference<Integer> confirmedCount = new AtomicReference<>(0);
-  private TestUntil until = TestUntil.happenings(0);
+  private AccessSafely access = afterCompleting(1);
+
+  private final List<Entry<String>> allEntries = new ArrayList<>();
+  private final List<State<String>> allSnapshots = new ArrayList<>();
 
   @Override
-  public synchronized void appended(final Entry<String> entry) {
-    allEntries.add(entry);
-    allSnapshots.add(State.TextState.Null);
-    until.happened();
+  public void appended(final Entry<String> entry) {
+    access.writeUsing("entry", Tuple2.from(entry, State.TextState.Null));
   }
 
   @Override
-  public synchronized void appendedWith(final Entry<String> entry, final State<String> snapshot) {
-    allEntries.add(entry);
-    allSnapshots.add(snapshot);
-    until.happened();
+  public void appendedWith(final Entry<String> entry, final State<String> snapshot) {
+    access.writeUsing("entry", Tuple2.from(entry, snapshot));
   }
 
   @Override
-  public synchronized void appendedAll(final List<Entry<String>> entries) {
-    allEntries.addAll(entries);
-    for (int idx = 0; idx < entries.size(); ++idx) {
-      allSnapshots.add(State.TextState.Null);
-    }
-    until.happened();
+  public void appendedAll(final List<Entry<String>> entries) {
+    access.writeUsing("entries", Tuple2.from(entries, snapshots(entries.size(), State.TextState.Null)));
   }
 
   @Override
-  public synchronized void appendedAllWith(final List<Entry<String>> entries, final State<String> snapshot) {
-    allEntries.addAll(entries);
-    for (int idx = 0; idx < (entries.size() - 1); ++idx) {
-      allSnapshots.add(State.TextState.Null);
-    }
-    allSnapshots.add(snapshot == null ? State.TextState.Null : snapshot);
-    until.happened();
+  public void appendedAllWith(final List<Entry<String>> entries, final State<String> snapshot) {
+    access.writeUsing("entries", Tuple2.from(entries, snapshots(entries.size(), snapshot)));
   }
 
-  public synchronized int confirmedCount() {
-    return confirmedCount.get();
+  public int confirmedCount() {
+    final int count = access.readFrom("count");
+    return count;
   }
 
-  public synchronized void confirmExpectedEntries(final int count, final int retries) {
-    for (int idx = 0; idx < retries; ++idx) {
-      until.completesWithin(100L);
-      final int peekCount = allEntries.size();
-      if (peekCount == count) {
-        confirmedCount.set(peekCount);
-        break;
-      }
-    }
-
-    if (confirmedCount.get() != count) {
-      final int peekCount = allEntries.size();
-      confirmedCount.set(peekCount);
-    }
-    until.happened();
+  public int confirmedCount(final int expected) {
+    final int count = access.readFromExpecting("count", expected);
+    return count;
   }
 
-  public synchronized Entry<String> entry(final int index) {
-    return allEntries.get(index);
+  public Entry<String> entry(final int index) {
+    final Entry<String> entry = access.readFrom("entry", index);
+    return entry;
   }
 
-  public synchronized TestUntil until(final int times) {
-    until.resetHappeningsTo(times);
-    return until;
+  public Entry<String> snapshot(final int index) {
+    final Entry<String> entry = access.readFrom("entry", index);
+    return entry;
+  }
+
+  public AccessSafely afterCompleting(final int times) {
+    access =
+            AccessSafely
+              .afterCompleting(times)
+              .writingWith("entry",
+                      (Tuple2<Entry<String>,State<String>> entry) -> {
+                        allEntries.add(entry._1);
+                        allSnapshots.add(entry._2);
+              })
+              .writingWith("entries",
+                      (Tuple2<Collection<Entry<String>>,Collection<State<String>>> entries) -> {
+                        allEntries.addAll(entries._1);
+                        allSnapshots.addAll(entries._2);
+              })
+              .readingWith("count", () -> allEntries.size())
+              .readingWith("snapshot", (Integer index) -> allSnapshots.get(index))
+              .readingWith("entry", (Integer index) -> allEntries.get(index));
+
+    return access;
+  }
+
+  private List<State<String>> snapshots(final int count, final State<String> snapshot) {
+    final State<String>[] array = new State.TextState[count];
+    Arrays.fill(array, State.TextState.Null);
+    array[array.length - 1] = (snapshot == null ? State.TextState.Null : snapshot);
+    final List<State<String>> snapshots = Arrays.asList(array);
+    return snapshots;
   }
 }
