7b300d83b7d2c0a859bd5677a90a1b23bb4bee64
VaughnVernon <vaughn@forcomprehension.com>
2019-02-11 16:55:43 -0700

    Reimplemented with await().

--- vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/forum/Fixtures.java
+++ vlingo-iddd-collaboration/src/test/java/com/saasovation/collaboration/model/forum/Fixtures.java
@@ -7,17 +7,13 @@
 
 package com.saasovation.collaboration.model.forum;
 
-import static org.junit.Assert.assertEquals;
-
 import com.saasovation.collaboration.model.Author;
 import com.saasovation.collaboration.model.Creator;
-import com.saasovation.collaboration.model.MockJournalListener;
 import com.saasovation.collaboration.model.Moderator;
 import com.saasovation.collaboration.model.Tenant;
 import com.saasovation.collaboration.model.forum.Forum.ForumDescription;
 
 import io.vlingo.actors.World;
-import io.vlingo.actors.testkit.TestUntil;
 import io.vlingo.common.Tuple2;
 
 public class Fixtures {
@@ -28,42 +24,23 @@ public class Fixtures {
     return new ForumDescription(Creator.unique(), Moderator.unique(), "All Things vlingo/platform", "Discuss the vlingo/platform");
   }
 
-  public Tuple2<DiscussionId, Discussion> discussionFixture(
-          final World world,
-          final MockJournalListener journalListener) {
-    final TestUntil until1 = journalListener.until(1);
+  public Tuple2<DiscussionId, Discussion> discussionFixture(final World world) {
     final Tuple2<ForumId,Forum> forumPair = Forum.startWith(world.stage(), Tenant.unique(), forumDescriptionFixture());
-    until1.completes();
-    journalListener.confirmExpectedEntries(1, 10);
-    assertEquals(1, journalListener.confirmedCount());
-    final TestUntil until2 = journalListener.until(2);
-    forumPair._2.discussFor(Author.unique(), "By Way of Discussion")
-      .andThenConsume(discussionPair -> {
-        this.discussionPair = discussionPair;
-        until2.happened();
-      });
-    until2.completes();
-    journalListener.confirmExpectedEntries(2, 10);
-    assertEquals(2, journalListener.confirmedCount());
+    
+    discussionPair = forumPair._2.discussFor(Author.unique(), "By Way of Discussion").await();
+
     return discussionPair;
   }
 
-  public Tuple2<PostId,Post> postFixture(
-          final World world,
-          final MockJournalListener journalListener) {
-    final TestUntil until1 = journalListener.until(1);
-    final Tuple2<DiscussionId, Discussion> discussionPair = discussionFixture(world, journalListener);
-    until1.completes();
-    final TestUntil until2 = journalListener.until(2);
+  public Tuple2<PostId,Post> postFixture(final World world) {
+    final Tuple2<DiscussionId, Discussion> discussionPair = discussionFixture(world);
+
     final Author author = Author.unique();
     final String subject = "Within the discussion a post";
     final String bodyText = "This is the body of the post which is document text.";
-    discussionPair._2.postFor(author, subject, bodyText)
-      .andThenConsume(postPair -> {
-        this.postPair = postPair;
-        until2.happened();
-      });
-    until2.completes();
+
+    postPair = discussionPair._2.postFor(author, subject, bodyText).await();
+
     return postPair;
   }
 }
