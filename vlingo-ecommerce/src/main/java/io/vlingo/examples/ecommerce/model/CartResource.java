da0923972dfe75ed965ba81b8724d52ba0df234e
Brian Wehrle <brian.wehrle@skyscanner.net>
2019-09-15 16:48:41 +0200

    Use ObjectResponse as return type from resource handler methods

--- vlingo-ecommerce/src/main/java/io/vlingo/examples/ecommerce/model/CartResource.java
+++ vlingo-ecommerce/src/main/java/io/vlingo/examples/ecommerce/model/CartResource.java
@@ -2,13 +2,14 @@ package io.vlingo.examples.ecommerce.model;
 
 import io.vlingo.actors.*;
 import io.vlingo.common.Completes;
-import io.vlingo.http.Body;
-import io.vlingo.http.Response;
+import io.vlingo.examples.ecommerce.model.Cart.CartItem;
+import io.vlingo.http.ResponseHeader;
+import io.vlingo.http.resource.ObjectResponse;
 import io.vlingo.http.resource.Resource;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static io.vlingo.common.serialization.JsonSerialization.serialized;
 import static io.vlingo.http.Response.Status.*;
 import static io.vlingo.http.ResponseHeader.*;
 import static io.vlingo.http.resource.ResourceBuilder.*;
@@ -23,33 +24,31 @@ public class CartResource {
         this.stage = world.stage();
     }
 
-    public Completes<Response> create(final UserId userId) {
+    public Completes<ObjectResponse<String>> create(final UserId userId) {
         final Address cartAddress = addressFactory.uniquePrefixedWith("cart-");
-        Cart cartActor = stage.actorFor(
+        final Cart cartActor = stage.actorFor(
                 Cart.class,
                 Definition.has(CartActor.class, Definition.parameters(cartAddress.idString())),
                 cartAddress);
 
         cartActor.createEmptyCartFor(userId);
 
-        return Completes.withSuccess(
-                Response.of(Created,
-                        headers(of(Location, urlLocation(cartAddress.idString()))),
-                        Body.from("")));
+        final Headers<ResponseHeader> headers = headers(of(Location, urlLocation(cartAddress.idString())));
+        return Completes.withSuccess(ObjectResponse.of(Created, headers, ""));
     }
 
-    public Completes<Response> queryCart(String cartId) {
+    public Completes<ObjectResponse<List<CartItem>>> queryCart(String cartId) {
         return stage.actorOf(Cart.class, addressFactory.from(cartId))
                 .andThenTo(Cart::queryCart)
-                .andThenTo(cartItems -> Completes.withSuccess(Response.of(Ok, serialized(cartItems))))
-                .otherwise(noCart -> Response.of(NotFound, urlLocation(cartId)));
+                .andThenTo(cartItems -> Completes.withSuccess(ObjectResponse.of(Ok, cartItems))
+                .otherwise(noCart -> ObjectResponse.of(NotFound, new ArrayList<>(0))));
     }
 
-    public Completes<Response> changeCartProductQuantity(String cartId, String idOfProduct, CartItemChange change) {
+    public Completes<ObjectResponse<List<CartItem>>> changeCartProductQuantity(String cartId, String idOfProduct, CartItemChange change) {
         return stage.actorOf(Cart.class, addressFactory.from(cartId))
                 .andThenTo(cart -> change.applyTo(cart, ProductId.fromId(idOfProduct)))
-                .andThenTo(cartItems -> Completes.withSuccess(Response.of(Ok, serialized(cartItems))))
-                .otherwise(noUser -> Response.of(NotFound, urlLocation(cartId)));
+                .andThenTo(cartItems -> Completes.withSuccess(ObjectResponse.of(Ok, cartItems)))
+                .otherwise(noUser -> ObjectResponse.of(NotFound, new ArrayList<>(0)));
     }
 
     private String urlLocation(final String shoppingCartId) {
@@ -80,7 +79,7 @@ public class CartResource {
             this.operation = operation;
         }
 
-        public Completes<List<Cart.CartItem>> applyTo(Cart cart, ProductId productId) {
+        public Completes<List<CartItem>> applyTo(Cart cart, ProductId productId) {
             if (operation.equals("add")) {
                 return cart.addItem(productId);
             } else if (operation.equals("remove")) {
