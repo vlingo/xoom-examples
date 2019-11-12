e6c06250d65ae566ae30dd79169227d596603406
VaughnVernon <vaughn@forcomprehension.com>
2019-11-12 18:12:29 +0100

    Tweaks.

--- vlingo-iddd-agilepm/src/main/java/com/saasovation/agilepm/infra/resource/ProductResource.java
+++ vlingo-iddd-agilepm/src/main/java/com/saasovation/agilepm/infra/resource/ProductResource.java
@@ -1,26 +1,36 @@
 package com.saasovation.agilepm.infra.resource;
 
+import static io.vlingo.http.Response.Status.Created;
+import static io.vlingo.http.Response.Status.InternalServerError;
+import static io.vlingo.http.Response.Status.NotFound;
+import static io.vlingo.http.Response.Status.Ok;
+import static io.vlingo.http.resource.ResourceBuilder.get;
+import static io.vlingo.http.resource.ResourceBuilder.patch;
+import static io.vlingo.http.resource.ResourceBuilder.post;
+import static io.vlingo.http.resource.ResourceBuilder.resource;
+
 import com.saasovation.agilepm.model.Tenant;
 import com.saasovation.agilepm.model.product.Product;
-import com.saasovation.agilepm.model.product.ProductEntity;
 import com.saasovation.agilepm.model.product.ProductId;
 import com.saasovation.agilepm.model.product.ProductOwner;
-import io.vlingo.actors.*;
+
+import io.vlingo.actors.AddressFactory;
+import io.vlingo.actors.Stage;
+import io.vlingo.actors.World;
 import io.vlingo.common.Completes;
+import io.vlingo.common.Tuple2;
 import io.vlingo.common.identity.IdentityGeneratorType;
 import io.vlingo.common.serialization.JsonSerialization;
 import io.vlingo.http.Response;
 import io.vlingo.http.resource.Resource;
 import io.vlingo.lattice.grid.GridAddressFactory;
 
-import static io.vlingo.http.Response.Status.Created;
-import static io.vlingo.http.Response.Status.InternalServerError;
-import static io.vlingo.http.resource.ResourceBuilder.post;
-import static io.vlingo.http.resource.ResourceBuilder.resource;
-
 public class ProductResource {
 
-    private static final String ROOT_URL = "/product";
+    private static final String PRODUCTS_URL = "/products";
+    private static final String PRODUCT_URL = "/tenants/{tenantId}/products/{productId}";
+    private static final String PRODUCT_QUERY_URL = "/products/{productId}";
+    
     private final Stage stage;
     private final AddressFactory addressFactory;
 
@@ -31,37 +41,60 @@ public class ProductResource {
 
     public Resource<?> routes() {
         return resource("Product resource fluent api",
-                post(ROOT_URL)
-                        .body(ProductCreateRequest.class)
-                        .handle(this::create));
+                post(PRODUCTS_URL)
+                        .body(ProductDefinition.class)
+                        .handle(this::defineWith),
+                patch(PRODUCT_URL)
+                        .param(String.class)
+                        .param(String.class)
+                        .body(String.class)
+                        .handle(this::changeDescription),
+                get(PRODUCT_QUERY_URL)
+                        .param(String.class)
+                        .handle(this::query));
     }
 
-    private Completes<Response> create(final ProductCreateRequest request) {
+    private Completes<Response> defineWith(final ProductDefinition productDefinition) {
         try {
-            final ProductId productId = ProductId.unique();
-            final Address productAddress = addressFactory.from(productId.id);
-            final Tenant tenant = Tenant.fromExisting(request.tenantId);
-            final Definition definition = Definition.has(ProductEntity.class, Definition.parameters(tenant, productId));
-
-            final Product product = stage.actorFor(Product.class, definition, productAddress);
-            product.define(ProductOwner.with(tenant, request.ownerId), request.name, request.description, request.hasDiscussion);
-
-            return Completes.withSuccess(Response.of(Created, JsonSerialization.serialized(productId)));
+          final Tuple2<ProductId, Product> product =
+                  Product.defineWith(
+                    stage,
+                    Tenant.fromExisting(productDefinition.tenantId),
+                    ProductOwner.fromExisting(productDefinition.tenantId, productDefinition.ownerId),
+                    productDefinition.name,
+                    productDefinition.description,
+                    productDefinition.hasDiscussion);
+          
+            return Completes.withSuccess(Response.of(Created, JsonSerialization.serialized(product._1)));
         } catch (Throwable t) {
             this.stage.world().defaultLogger().error("Failed to create the product", t);
             return Completes.withSuccess(Response.of(InternalServerError));
         }
     }
 
+    private Completes<Response> changeDescription(String tenantId, String productId, String description) {
+      return stage.actorOf(Product.class, addressFactory.from(productId))
+              .andThenTo(product -> product.changeDescription(description))
+              .andThenTo(product -> Completes.withSuccess(Response.of(Ok))
+              .otherwise(noProduct -> Response.of(NotFound, productId)));
+    }
 
-    public static class ProductCreateRequest {
+    private Completes<Response> query(String productId) {
+      stage.world().defaultLogger().debug("GET Product: " + productId);
+      return stage.actorOf(Product.class, addressFactory.from(productId))
+              .andThenTo(product -> product.query())
+              .andThenTo(state -> Completes.withSuccess(Response.of(Ok, JsonSerialization.serialized(state)))
+              .otherwise(noProduct -> Response.of(NotFound, productId)));
+    }
+    
+    public static class ProductDefinition {
         final String tenantId;
         final String ownerId;
         final String name;
         final String description;
         final boolean hasDiscussion;
 
-        ProductCreateRequest(final String tenantId, final String ownerId, final String name, final String description, final boolean hasDiscussion) {
+        ProductDefinition(final String tenantId, final String ownerId, final String name, final String description, final boolean hasDiscussion) {
             this.tenantId = tenantId;
             this.ownerId = ownerId;
             this.name = name;
--- vlingo-iddd-agilepm/src/main/java/com/saasovation/agilepm/model/product/ProductEntity.java
+++ vlingo-iddd-agilepm/src/main/java/com/saasovation/agilepm/model/product/ProductEntity.java
@@ -8,7 +8,14 @@
 package com.saasovation.agilepm.model.product;
 
 import com.saasovation.agilepm.model.Tenant;
-import com.saasovation.agilepm.model.product.Events.*;
+import com.saasovation.agilepm.model.product.Events.ProductDefined;
+import com.saasovation.agilepm.model.product.Events.ProductDescriptionChanged;
+import com.saasovation.agilepm.model.product.Events.ProductDiscussionAttached;
+import com.saasovation.agilepm.model.product.Events.ProductDiscussionRequested;
+import com.saasovation.agilepm.model.product.Events.ProductNameChanged;
+import com.saasovation.agilepm.model.product.Events.ProductOwnerAssigned;
+
+import io.vlingo.common.Completes;
 import io.vlingo.lattice.model.sourcing.EventSourced;
 
 public class ProductEntity extends EventSourced implements Product {
@@ -36,10 +43,11 @@ public class ProductEntity extends EventSourced implements Product {
         }
     }
 
-    public void changeDescription(final String description) {
+    public Completes<String> changeDescription(final String description) {
         if (!state.description.equals(description)) {
-            apply(ProductDescriptionChanged.with(state.tenant, state.productId, description));
+            apply(ProductDescriptionChanged.with(state.tenant, state.productId, description), () -> description);
         }
+        return completes().with(description);
     }
 
     public void changeName(final String name) {
@@ -48,13 +56,16 @@ public class ProductEntity extends EventSourced implements Product {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public void define(final ProductOwner productOwner, final String name, final String description, final boolean hasDiscussion) {
         if (state.productOwner == null) {
+          if (hasDiscussion) {
+            logger().debug("apply(): Product id:" + state.productId.id);
+            apply(asList(ProductDefined.with(state.tenant, state.productId, productOwner, name, description, hasDiscussion),
+                         ProductDiscussionRequested.with(state.tenant, state.productId, productOwner, name, description)));
+          } else {
             apply(ProductDefined.with(state.tenant, state.productId, productOwner, name, description, hasDiscussion));
-
-            if (hasDiscussion) {
-                apply(ProductDiscussionRequested.with(state.tenant, state.productId, productOwner, name, description));
-            }
+          }
         }
     }
 
@@ -62,6 +73,11 @@ public class ProductEntity extends EventSourced implements Product {
         if (!state.hasDiscussion) {
             apply(ProductDiscussionRequested.with(state.tenant, state.productId, state.productOwner, state.name, state.description));
         }
+    }
+
+    @Override
+    public Completes<State> query() {
+      return completes().with(state);
     }
 
     @Override
