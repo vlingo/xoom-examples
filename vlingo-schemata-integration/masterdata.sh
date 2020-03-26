d212363d56df375c1c364e6427cf420183177aa7
Wolfgang Werner <mail@wolfgang-werner.net>
2020-03-26 13:59:36 +0100

    Clean up masterdata setup script and make more robust

--- vlingo-schemata-integration/masterdata.sh
+++ vlingo-schemata-integration/masterdata.sh
@@ -3,16 +3,54 @@
 # Requires curl and jq to be on the path, vlingo-schemata runnning on localhost and
 # VLINGO_SCHEMATA_PORT being set.
 
-ORG_ID=$(curl -s -d '{ "organizationId": "", "name": "Vlingo", "description": "Vlingo Organization" }' -H 'Content-Type: application/json' -X POST http://localhost:${VLINGO_SCHEMATA_PORT}/organizations | jq -r '.organizationId')
-echo Created organization ${ORG_ID}
+set -e
 
-UNIT_ID=$(curl -s -d '{ "organizationId": "'${ORG_ID}'", "unitId": "", "name": "examples", "description": "Examples for vlingo/schemata" }' -X POST -H 'Content-Type: application/json' http://localhost:${VLINGO_SCHEMATA_PORT}/organizations/${ORG_ID}/units | jq -r '.unitId')
-echo Created unit ${UNIT_ID}
+fail () {
+  echo 'failed.'
+  exit 1
+}
 
-CONTEXT_ID=$(curl -s -X POST -H 'Content-Type: application/json' -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "", "namespace": "io.vlingo.examples.schemata", "description": "Bounded context for vlingo/schemata examples" }' http://localhost:${VLINGO_SCHEMATA_PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts | jq -r '.contextId')
+[ "$VLINGO_SCHEMATA_PORT" != "" ] || (echo 'Please set $VLINGO_SCHEMATA_PORT.' && fail)
+
+PORT=$VLINGO_SCHEMATA_PORT
+
+echo Creating organization ...
+ORG=$(curl -s \
+  -d '{ "organizationId": "", "name": "Vlingo", "description": "Vlingo Organization" }' \
+  -H 'Content-Type: application/json' \
+  -X POST http://localhost:${PORT}/organizations) \
+  || fail
+ORG_ID=$(echo $ORG  | jq -r '.organizationId')
+echo Created organization ${ORG_ID}.
+
+echo Creating unit ...
+UNIT=$(curl -s \
+  -d '{ "organizationId": "'${ORG_ID}'", "unitId": "", "name": "examples", "description": "Examples for vlingo/schemata" }' \
+  -H 'Content-Type: application/json' \
+  -X POST http://localhost:${PORT}/organizations/${ORG_ID}/units) \
+  || fail
+UNIT_ID=$(echo $UNIT  | jq -r '.unitId')
+echo Created unit ${UNIT_ID}.
+
+echo Creating context ${ORG_ID}
+CONTEXT=$(curl -s \
+  -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "", "namespace": "io.vlingo.examples.schemata", "description": "Bounded context for vlingo/schemata examples" }' \
+  -H 'Content-Type: application/json' \
+  -X POST http://localhost:${PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts) \
+  || fail
+CONTEXT_ID=$(echo $CONTEXT| jq -r '.contextId')
 echo Created context ${CONTEXT_ID}
 
-curl -s -X POST -H 'Content-Type: application/json' -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "'${CONTEXT_ID}'", "schemaId": "", "category": "Event", "name": "SchemaDefined", "scope": "Public", "description": "Fired whenever a new schema is defined." }' http://localhost:${VLINGO_SCHEMATA_PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts/${CONTEXT_ID}/schemas | jq
-curl -s -X POST -H 'Content-Type: application/json' -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "'${CONTEXT_ID}'", "schemaId": "", "category": "Event", "name": "SchemaPublished", "scope": "Public", "description": "Fired whenever a schema version is published." }' http://localhost:${VLINGO_SCHEMATA_PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts/${CONTEXT_ID}/schemas | jq
+echo Creating schemata ...
+curl -s \
+  -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "'${CONTEXT_ID}'", "schemaId": "", "category": "Event", "name": "SchemaDefined", "scope": "Public", "description": "Fired whenever a new schema is defined." }' \
+  -H 'Content-Type: application/json' \
+  -X POST http://localhost:${PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts/${CONTEXT_ID}/schemas | jq
+
+curl -s \
+  -d '{ "organizationId": "'${ORG_ID}'", "unitId": "'${UNIT_ID}'", "contextId": "'${CONTEXT_ID}'", "schemaId": "", "category": "Event", "name": "SchemaPublished", "scope": "Public", "description": "Fired whenever a schema version is published." }' \
+  -H 'Content-Type: application/json' \
+  -X POST http://localhost:${PORT}/organizations/${ORG_ID}/units/${UNIT_ID}/contexts/${CONTEXT_ID}/schemas | jq
+
 echo Created schemata.
 echo Done.
