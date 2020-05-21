311f7fe1a1207b76cf43a60ce6316c15a61385df
VaughnVernon <vaughn@forcomprehension.com>
2020-05-21 00:45:27 -0700

    Initial revision of Pi.

---
+++ vlingo-calculate-pi/src/main/java/io/vlingo/pi/calculator/CalculatorState.java
@@ -0,0 +1,44 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.pi.calculator;
+
+import io.vlingo.actors.CompletesEventually;
+import io.vlingo.actors.Stage;
+
+final class CalculatorState {
+  CompletesEventually completes;
+  final PartitionCalculationInterest interest;
+  double pi;
+  int resultsCount;
+  final int totalPartitionValues;
+  final int totalMessages;
+  int workerIndex;
+  PartitionCalculator[] workers;
+
+  CalculatorState(final Stage stage, final PartitionCalculationInterest interest, final int totalWorkers, final int totalPartitionValues, final int totalMessages) {
+    this.totalPartitionValues = totalPartitionValues;
+    this.totalMessages = totalMessages;
+    this.pi = 0;
+    this.resultsCount = 0;
+
+    this.workerIndex = 0;
+    this.workers = workers(stage, totalWorkers);
+    
+    this.interest = interest;
+  }
+
+  private PartitionCalculator[] workers(final Stage stage, final int totalWorkers) {
+    final PartitionCalculator[] workers = new PartitionCalculator[totalWorkers];
+
+    for (int idx = 0; idx < totalWorkers; ++idx) {
+      workers[idx] = PartitionCalculator.on(stage);
+    }
+
+    return workers;
+  }
+}
---
+++ vlingo-calculate-pi/src/main/java/io/vlingo/pi/calculator/Pi.java
@@ -0,0 +1,51 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.pi.calculator;
+
+import java.util.concurrent.CountDownLatch;
+
+import io.vlingo.actors.World;
+
+public class Pi {
+  public static void main(final String[] args) throws Exception {
+    final Pi pi = Pi.calculatorWith(4, 10_000, 10_000);
+
+    pi.calculate();
+  }
+
+  private final Calculator calculator;
+
+  private static Pi calculatorWith(final int totalWorkers, final int totalPartitionValues, final int totalMessages) {
+    final World world = World.startWithDefaults("Pi");
+
+    final Calculator calculator = Calculator.with(world.stage(), totalWorkers, totalPartitionValues, totalMessages);
+
+    return new Pi(calculator);
+  }
+
+  private Pi(final Calculator calculator) {
+    this.calculator = calculator;
+  }
+
+  private void calculate() throws Exception {
+    final CountDownLatch latch = new CountDownLatch(1);
+    final long startTime = System.currentTimeMillis();
+
+    calculator.calculate().andFinallyConsume(pi -> {
+      final long totalTime = System.currentTimeMillis() - startTime;
+
+      System.out.println("Pi: " + pi + " Time: " + totalTime + " ms");
+
+      latch.countDown();
+    });
+
+    latch.await();
+    System.out.println("Done.");
+    System.exit(0);
+  }
+}
