407c11753f92d73a65e49c9d105e89b4671a0b39
VaughnVernon <vaughn@forcomprehension.com>
2020-05-21 17:12:41 -0700

    PingPong main() game() play().

---
+++ vlingo-actors-playground/src/main/java/playground/PingPong.java
@@ -0,0 +1,50 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package playground;
+
+import io.vlingo.actors.World;
+import io.vlingo.actors.testkit.TestUntil;
+
+public class PingPong {
+  public static void main(final String[] args) throws Exception {
+    final PingPong pingPong = PingPong.game();
+
+    pingPong.play();
+  }
+
+  private final Pinger pinger;
+  private final Ponger ponger;
+  private final TestUntil until;
+  private final World world;
+
+  private static PingPong game() {
+    final World world = World.startWithDefaults("ping-pong");
+    final TestUntil until = TestUntil.happenings(1);
+    final Pinger pinger = world.actorFor(Pinger.class, PingerActor.class, until);
+    final Ponger ponger = world.actorFor(Ponger.class, PongerActor.class);
+
+    return new PingPong(world, until, pinger, ponger);
+  }
+
+  private void play() {
+    pinger.ping(ponger);
+
+    until.completes();
+
+    world.terminate();
+
+    System.exit(0);
+  }
+
+  private PingPong(final World world, final TestUntil until, final Pinger pinger, final Ponger ponger) {
+    this.world = world;
+    this.until = until;
+    this.pinger = pinger;
+    this.ponger = ponger;
+  }
+}
