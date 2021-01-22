07433ec51d7f7d438b74b406886bc327e95fec56
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-01-22 17:39:05 -0300

    Handling Projection

--- vlingo-e2e-sys-airline-ops/inventory/src/main/java/com/vgoairlines/inventory/infrastructure/persistence/AircraftProjectionActor.java
+++ vlingo-e2e-sys-airline-ops/inventory/src/main/java/com/vgoairlines/inventory/infrastructure/persistence/AircraftProjectionActor.java
@@ -2,13 +2,12 @@ package com.vgoairlines.inventory.infrastructure.persistence;
 
 import com.vgoairlines.inventory.infrastructure.AircraftData;
 import com.vgoairlines.inventory.infrastructure.Events;
-
+import com.vgoairlines.inventory.model.aircraft.AircraftState;
 import io.vlingo.lattice.model.projection.Projectable;
 import io.vlingo.lattice.model.projection.StateStoreProjectionActor;
 import io.vlingo.symbio.Source;
 
 public class AircraftProjectionActor extends StateStoreProjectionActor<AircraftData> {
-  private static final AircraftData Empty = AircraftData.empty();
 
   public AircraftProjectionActor() {
     super(QueryModelStateStoreProvider.instance().store);
@@ -16,7 +15,8 @@ public class AircraftProjectionActor extends StateStoreProjectionActor<AircraftD
 
   @Override
   protected AircraftData currentDataFor(final Projectable projectable) {
-    return Empty;
+    final AircraftState aircraftState = projectable.object();
+    return AircraftData.from(aircraftState);
   }
 
   @Override
@@ -29,7 +29,7 @@ public class AircraftProjectionActor extends StateStoreProjectionActor<AircraftD
     for (final Source<?> event : sources()) {
       switch (Events.valueOf(event.typeName())) {
         case AircraftConsigned:
-          return AircraftData.empty();   // TODO: implement actual merge
+          return currentData;   // TODO: implement actual merge
         default:
           logger().warn("Event of type " + event.typeName() + " was not matched.");
           break;
