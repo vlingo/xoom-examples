0daa2ed2c322cb0a31b958ae93cd9c7245c3dbec
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-01-30 15:11:55 -0300

    Fleet Crew / ATC integration

--- vlingo-e2e-sys-airline-ops/air-traffic-control/src/main/java/com/skyharbor/airtrafficcontrol/infrastructure/resource/FlightResourceHandlers.java
+++ vlingo-e2e-sys-airline-ops/air-traffic-control/src/main/java/com/skyharbor/airtrafficcontrol/infrastructure/resource/FlightResourceHandlers.java
@@ -1,15 +1,16 @@
 package com.skyharbor.airtrafficcontrol.infrastructure.resource;
 
+import com.skyharbor.airtrafficcontrol.infrastructure.FlightData;
+import com.skyharbor.airtrafficcontrol.infrastructure.persistence.FlightQueries;
+import com.skyharbor.airtrafficcontrol.model.flight.Flight;
+import com.skyharbor.airtrafficcontrol.model.flight.FlightState;
+import com.skyharbor.airtrafficcontrol.model.flight.FlightStatus;
 import io.vlingo.actors.Stage;
 import io.vlingo.common.Completes;
 import io.vlingo.xoom.annotation.autodispatch.Handler.Three;
 import io.vlingo.xoom.annotation.autodispatch.Handler.Two;
 import io.vlingo.xoom.annotation.autodispatch.HandlerEntry;
 
-import com.skyharbor.airtrafficcontrol.model.flight.FlightState;
-import com.skyharbor.airtrafficcontrol.infrastructure.persistence.FlightQueries;
-import com.skyharbor.airtrafficcontrol.infrastructure.FlightData;
-import com.skyharbor.airtrafficcontrol.model.flight.Flight;
 import java.util.Collection;
 
 public class FlightResourceHandlers {
@@ -17,9 +18,18 @@ public class FlightResourceHandlers {
   public static final int DEPART_GATE = 0;
   public static final int FLIGHTS = 1;
   public static final int ADAPT_STATE = 2;
+  public static final int CHANGE_STATUS = 3;
 
   public static final HandlerEntry<Three<Completes<FlightState>, Stage, FlightData>> DEPART_GATE_HANDLER =
           HandlerEntry.of(DEPART_GATE, ($stage, data) -> Flight.departGate($stage, data.number, data.tailNumber, data.equipment));
+
+  public static final HandlerEntry<Three<Completes<FlightState>, Flight, FlightData>> CHANGE_STATUS_HANDLER =
+          HandlerEntry.of(CHANGE_STATUS, (flight, data) -> {
+            if(FlightStatus.valueOf(data.status).isLanded()) {
+              return flight.land(data.number);
+            }
+            throw new UnsupportedOperationException("Unable to change Flight Status to " + data.status);
+          });
 
   public static final HandlerEntry<Two<FlightData, FlightState>> ADAPT_STATE_HANDLER =
           HandlerEntry.of(ADAPT_STATE, FlightData::from);
---
+++ vlingo-e2e-sys-airline-ops/fleetcrew/src/main/java/com/skyharbor/fleetcrew/infrastructure/exchange/SchemaReferences.java
@@ -0,0 +1,22 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+package com.skyharbor.fleetcrew.infrastructure.exchange;
+
+public enum SchemaReferences {
+
+  FLIGHT_LANDED("SkyHarborPHX:groundops:com.skyharbor.airtrafficcontrol:FlightLanded:1.0.0");
+
+  public final String value;
+
+  SchemaReferences(final String value) {
+    this.value = value;
+  }
+
+  public boolean match(final String schemaName) {
+    return this.value.equals(schemaName);
+  }
+}
