cb7109156a1f706d56e906fbd790824b63731310
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-02-01 12:37:35 -0300

    Including aircraftId

--- vlingo-e2e-sys-airline-ops/airport-terminal/src/main/java/com/vgoairlines/airportterminal/model/flight/FlightState.java
+++ vlingo-e2e-sys-airline-ops/airport-terminal/src/main/java/com/vgoairlines/airportterminal/model/flight/FlightState.java
@@ -6,23 +6,26 @@ public final class FlightState extends StateObject {
 
   public final String id;
   public final String number;
+  public final String aircraftId;
   public final GateAssignment gateAssignment;
   public final Equipment equipment;
   public final Schedule schedule;
   public final Status status;
 
   public static FlightState identifiedBy(final String id) {
-    return new FlightState(id, null, null, null, null, null);
+    return new FlightState(id, null, null, null, null, null, null);
   }
 
-  public FlightState (final String id,
-                      final String number,
-                      final GateAssignment gateAssignment,
-                      final Equipment equipment,
-                      final Schedule schedule,
-                      final Status status) {
+  public FlightState(final String id,
+                     final String number,
+                     final String aircraftId,
+                     final GateAssignment gateAssignment,
+                     final Equipment equipment,
+                     final Schedule schedule,
+                     final Status status) {
     this.id = id;
     this.number = number;
+    this.aircraftId = aircraftId;
     this.gateAssignment = gateAssignment;
     this.equipment = equipment;
     this.schedule = schedule;
@@ -33,27 +36,27 @@ public final class FlightState extends StateObject {
                               final GateAssignment gateAssignment,
                               final Equipment equipment,
                               final Schedule schedule) {
-    return new FlightState(this.id, number, gateAssignment, equipment, schedule, Status.GATE_OPENED);
+    return new FlightState(this.id, number, aircraftId, gateAssignment, equipment, schedule, Status.GATE_OPENED);
   }
 
   public FlightState arrive(final Schedule schedule) {
-    return new FlightState(this.id, this.number, this.gateAssignment, this.equipment, schedule, Status.ARRIVED);
+    return new FlightState(this.id, this.number, this.aircraftId, this.gateAssignment, this.equipment, schedule, Status.ARRIVED);
   }
 
   public FlightState startBoarding() {
-    return new FlightState(this.id, this.number, this.gateAssignment, this.equipment, this.schedule, Status.BOARDING);
+    return new FlightState(this.id, this.number, this.aircraftId, this.gateAssignment, this.equipment, this.schedule, Status.BOARDING);
   }
 
   public FlightState endBoarding() {
-    return new FlightState(this.id, this.number, this.gateAssignment, this.equipment, this.schedule, Status.BOARDED);
+    return new FlightState(this.id, this.number, this.aircraftId, this.gateAssignment, this.equipment, this.schedule, Status.BOARDED);
   }
 
   public FlightState depart(final Schedule schedule) {
-    return new FlightState(this.id, this.number, this.gateAssignment, this.equipment, schedule, Status.DEPARTED);
+    return new FlightState(this.id, this.number, aircraftId, this.gateAssignment, this.equipment, schedule, Status.DEPARTED);
   }
 
   public FlightState closeGate() {
-    return new FlightState(this.id, this.number, this.gateAssignment, this.equipment, this.schedule, Status.GATE_CLOSED);
+    return new FlightState(this.id, this.number, this.aircraftId, this.gateAssignment, this.equipment, this.schedule, Status.GATE_CLOSED);
   }
 
 }
