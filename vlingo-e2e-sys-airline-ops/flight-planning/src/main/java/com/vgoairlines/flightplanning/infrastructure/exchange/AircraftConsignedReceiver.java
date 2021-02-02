9f3da72bff94934c506cf8f21dd97e9afe44eb58
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-02-02 01:18:52 -0300

    FP / AI integration

--- vlingo-e2e-sys-airline-ops/flight-planning/src/main/java/com/vgoairlines/flightplanning/infrastructure/exchange/AircraftConsignedReceiver.java
+++ vlingo-e2e-sys-airline-ops/flight-planning/src/main/java/com/vgoairlines/flightplanning/infrastructure/exchange/AircraftConsignedReceiver.java
@@ -4,6 +4,7 @@
 // Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
+
 package com.vgoairlines.flightplanning.infrastructure.exchange;
 
 import com.vgoairlines.flightplanning.model.aircraft.Aircraft;
