0a7f9f207e67ad8a34c01bf5a78cf046360a9c1c
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-02-03 06:24:47 -0300

    Code Review

--- vlingo-e2e-sys-airline-ops/flight-planning/src/main/java/com/vgoairlines/flightplanning/model/aircraft/AircraftEntity.java
+++ vlingo-e2e-sys-airline-ops/flight-planning/src/main/java/com/vgoairlines/flightplanning/model/aircraft/AircraftEntity.java
@@ -1,9 +1,10 @@
-// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the
 // Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
+
 package com.vgoairlines.flightplanning.model.aircraft;
 
 import io.vlingo.common.Completes;
@@ -20,6 +21,7 @@ public class AircraftEntity extends StatefulEntity<AircraftState> implements Air
 
     @Override
     public Completes<AircraftState> pool(final String aircraftId, final Denomination denomination) {
+        logger().info("Aircraft " + aircraftId + " is going to be pooled");
         final AircraftState stateArg = state.pool(aircraftId, denomination);
         return apply(stateArg, new AircraftPooled(stateArg), () -> state);
     }
