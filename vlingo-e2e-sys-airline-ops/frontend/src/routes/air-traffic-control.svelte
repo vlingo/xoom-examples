0d02ee93a30b054e8cf9e0bcd4c430ae313d0378
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-10 22:19:03 +0300

    navigation and pages are created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ vlingo-e2e-sys-airline-ops/frontend/src/routes/air-traffic-control.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Air Traffic Control</title>
+</svelte:head>
+
+<h1>Air Traffic Control</h1>
---
+++ vlingo-e2e-sys-airline-ops/frontend/src/stores/index.js
@@ -0,0 +1,33 @@
+import { writable } from 'svelte/store';
+
+function isMobileStore() {
+	const { subscribe, set } = writable(false);
+	return {
+		subscribe,
+		set,
+		check: () => {
+			import('svelte-materialify/src/utils/breakpoints').then(({ default: breakpoints }) => {
+				set(window.matchMedia(breakpoints['md-and-down']).matches);
+			});
+		},
+	};
+};
+
+export const isMobile = isMobileStore();
+
+export function createLocalStore(key, initialValue) {
+	const localValue = process.browser ? localStorage.getItem(key) : initialValue;
+	const { subscribe, set } = writable(localValue);
+
+	return {
+		subscribe,
+		set: (value) => {
+			if (process.browser) {
+				localStorage.setItem(key, value);
+			}
+			set(value)
+		},
+	};
+}
+
+export const theme = createLocalStore('theme', 'light');
