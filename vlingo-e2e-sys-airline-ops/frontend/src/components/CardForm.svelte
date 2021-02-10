902a6195732f887540254a6399ff7501cdf61809
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-11 02:47:08 +0300

    card form, vlbutton and simple forms are  added to route pages
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ vlingo-e2e-sys-airline-ops/frontend/src/components/CardForm.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
+	import { Card, CardActions, CardText, CardTitle, Row, Col } from 'svelte-materialify/src';
+	import VlButton from './VlButton.svelte';
+
+	export let title = "";
+	export let nextLink = "";
+	export let prevLink = "";
+
+	export let isNextDisabled = false;
+</script>
+
+<Card class="vl-card pa-4 pa-md-8">
+	<CardTitle class="pa-0">
+		{title}
+	</CardTitle>
+	<CardText class="pa-0 mt-6 mb-6">
+		<slot/>
+	</CardText>
+	<CardActions class="pa-0">
+		<slot name="actions">
+			<Row class="justify-center">
+				{#if prevLink}
+				<Col cols="auto">
+						<VlButton icon={mdiChevronLeft} outlined text="Prev" href={prevLink} />
+				</Col>
+				{/if}
+				<Col cols="auto" class="text-center">
+					<VlButton icon={mdiChevronRight} outlined text="Next" href={nextLink} disabled={isNextDisabled} />
+				</Col>
+			</Row>
+		</slot>
+	</CardActions>
+</Card>
+
+<style>
+</style>
--- vlingo-e2e-sys-airline-ops/frontend/src/routes/flight-planning.svelte
+++ vlingo-e2e-sys-airline-ops/frontend/src/routes/flight-planning.svelte
@@ -2,4 +2,21 @@
 	<title>Flight Planning</title>
 </svelte:head>
 
-<h1>Flight Planning</h1>
+<script>
+	import { TextField, Select } from 'svelte-materialify/src';
+	import CardForm from '../components/CardForm.svelte';
+
+	const aircrafts = [
+    { name: 'AirCraft1', value: 'AirCraft1' },
+    { name: 'AirCraft2', value: 'AirCraft2' },
+  ];
+	let valid = false;
+</script>
+
+<CardForm title="Flight Planning" prevLink="inventory" nextLink="fleet-crew" isNextDisabled={false}>
+	<Select outlined items={aircrafts}>Aircraft</Select>
+	<TextField outlined>Departure Date</TextField>
+	<TextField outlined>Departure Airport</TextField>
+	<TextField outlined>Arrival Date</TextField>
+	<TextField outlined>Arrival Airport</TextField>
+</CardForm>
