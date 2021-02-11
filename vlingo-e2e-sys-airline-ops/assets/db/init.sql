842e73126a9c88d30f9a0932e749153d878ff649
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-02-11 14:58:17 -0300

    Infrastructure Containers

---
+++ vlingo-e2e-sys-airline-ops/assets/db/init.sql
@@ -0,0 +1,17 @@
+create database "vgo-airlines-air-traffic-control";
+alter database "vgo-airlines-air-traffic-control" owner to vlingo;
+
+create database "vgo-airlines-aircraft-monitoring";
+alter database "vgo-airlines-aircraft-monitoring" owner to vlingo;
+
+create database "vgo-airlines-airport-terminal";
+alter database "vgo-airlines-airport-terminal" owner to vlingo;
+
+create database "vgo-airlines-fleet-crew";
+alter database "vgo-airlines-fleet-crew" owner to vlingo;
+
+create database "vgo-airlines-flight-planning";
+alter database "vgo-airlines-flight-planning" owner to vlingo;
+
+create database "vgo-airlines-inventory";
+alter database "vgo-airlines-inventory" owner to vlingo;
