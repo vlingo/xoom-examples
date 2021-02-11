51ee2517bd2501ca9f11b2ec79c268d51f8559d7
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-12 00:21:11 +0300

    inventory form variable is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- vlingo-e2e-sys-airline-ops/frontend/src/routes/inventory.svelte
+++ vlingo-e2e-sys-airline-ops/frontend/src/routes/inventory.svelte
@@ -6,18 +6,38 @@
 	import { TextField, Select } from 'svelte-materialify/src';
 	import CardForm from '../components/CardForm.svelte';
 
-	const carrierType = [
+	const carrierTypes = [
     { name: 'Airline', value: 'AIRLINE' },
     { name: 'Shipping', value: 'SHIPPING' },
   ];
 	let valid = false;
+	let manufacturer = "EMB";
+	let model = "E1-190";
+	let serialNumber = "1515";
+	let carrierName = "TAM";
+	let carrierType = ["AIRLINE"];
+	let tailNumber = "VGO1616";
+	$: formData = {
+		registration: {
+			tailNumber
+		},
+		manufacturerSpecification : {
+			manufacturer,
+			model,
+			serialNumber
+		},
+		carrier: {
+			name: carrierName,
+			type: carrierType
+		}
+	}
 </script>
 
 <CardForm title="Inventory" nextLink="flight-planning" isNextDisabled={false}>
-	<TextField outlined>Manufacturer</TextField>
-	<TextField outlined>Model</TextField>
-	<TextField outlined>Serial Number</TextField>
-	<TextField outlined>Carrier</TextField>
-	<Select multiple outlined items={carrierType}>Carrier Type</Select>
-	<TextField outlined>Tail Number</TextField>
+	<TextField outlined bind:value={manufacturer}>Manufacturer</TextField>
+	<TextField outlined bind:value={model}>Model</TextField>
+	<TextField outlined bind:value={serialNumber}>Serial Number</TextField>
+	<TextField outlined bind:value={tailNumber}>Tail Number</TextField>
+	<TextField outlined bind:value={carrierName}>Carrier Name</TextField>
+	<Select multiple outlined items={carrierTypes} bind:value={carrierType}>Carrier Type</Select>
 </CardForm>
