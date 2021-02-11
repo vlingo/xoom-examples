5461be0b4dd3f06967f0e0bb21f2b28c8c66d47a
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-12 00:22:18 +0300

    airport terminal form inputs and form variable is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- vlingo-e2e-sys-airline-ops/frontend/src/routes/airport-terminal.svelte
+++ vlingo-e2e-sys-airline-ops/frontend/src/routes/airport-terminal.svelte
@@ -2,4 +2,51 @@
 	<title>Airport Terminal</title>
 </svelte:head>
 
-<h1>Airport Terminal</h1>
+<script>
+	import { TextField, Select } from 'svelte-materialify/src';
+	import CardForm from '../components/CardForm.svelte';
+
+	const aircrafts = [
+    { name: 'AirCraft1', value: 'AirCraft1' },
+    { name: 'AirCraft2', value: 'AirCraft2' },
+  ];
+
+	let valid = false;
+	let formData = {
+		number: "1983",
+		gateAssignment: {
+			terminal:"A1",
+	    number: "12"
+		},
+		equipment: {
+		  carrier: "EMB",
+	    tailNumber: "2011"
+		},
+		schedule: {
+			scheduledDeparture : 1613066697198,
+	    scheduledArrival: 1613066908474,
+	    departureStatus: {
+				actual: 1613066918474
+    	}
+		}
+	}
+
+</script>
+
+<CardForm title="Airport Terminal" prevLink="fleet-crew" nextLink="air-traffic-control" isNextDisabled={false}>
+	<TextField outlined>Gate Agent Name</TextField>
+	<Select outlined items={aircrafts}>Flight</Select>
+
+	<TextField outlined>Equipment - Terminal</TextField>
+	<TextField outlined>Equipment - Number</TextField>
+
+	<TextField outlined>Gate Assignment - Terminal</TextField>
+	<TextField outlined>Gate Assignment - Number</TextField>
+
+	<TextField outlined>Scheduled Departure</TextField>
+	<TextField outlined>Scheduled Arrival</TextField>
+
+	<TextField outlined>Departure Status - Delayed By</TextField>
+	<TextField outlined>Departure Status - Actual</TextField>
+</CardForm>
+
