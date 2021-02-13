f4954e896340632dc0083f3fa5eb52fd8375f0da
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-13 21:17:25 +0300

    flight terminal page table and dialog form is added
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- vlingo-e2e-sys-airline-ops/frontend/src/routes/airport-terminal.svelte
+++ vlingo-e2e-sys-airline-ops/frontend/src/routes/airport-terminal.svelte
@@ -3,58 +3,103 @@
 </svelte:head>
 
 <script>
-	import { TextField, Select } from 'svelte-materialify/src';
+	import { TextField, Button, Dialog, Row } from 'svelte-materialify/src';
 	import CardForm from '../components/CardForm.svelte';
-
-	const aircrafts = [
-    { name: 'AirCraft1', value: 'AirCraft1' },
-    { name: 'AirCraft2', value: 'AirCraft2' },
-  ];
-
+	import { Api } from "../api";
+	import { terminals } from "../stores/airport-terminal.js"
+	import { onMount } from 'svelte';
+	let isDialogActive = false;
 	let valid = false;
+
 	let formData = {
 		number: "1983",
 		gateAssignment: {
-			terminal:"A1",
-	    number: "12"
+			terminal: "A1",
+			number: "12"
 		},
 		equipment: {
-		  carrier: "EMB",
-	    tailNumber: "2011"
+			carrier: "EMB",
+			tailNumber: "2011"
 		},
 		schedule: {
-			scheduledDeparture : 1613066697198,
-	    scheduledArrival: 1613066908474,
-	    departureStatus: {
+			scheduledDeparture: 1613066697198,
+			scheduledArrival: 1613066908474,
+			departureStatus: {
 				actual: 1613066918474
-    	}
+			}
+		}
+	}
+	const submit= async () => {
+		const res = await Api.post("/flights/", formData);
+		if (res) {
+			$terminals = [...$terminals, res]
+			toggleDialog()
 		}
 	}
 
+	onMount(async () => {
+		$terminals = await Api.get("/flights/");
+	})
 
-	const submit= async () => {
-		const res = await Api.post("/flights", formData);
-		console.log(res);
+	const toggleDialog = () => {
+		isDialogActive = !isDialogActive;
 	}
 
 </script>
 
 <CardForm title="Airport Terminal" prevLink="fleet-crew" nextLink="air-traffic-control" isNextDisabled={false}>
-	<form on:submit|preventDefault={submit}>
-		<TextField outlined>Gate Agent Name</TextField>
-		<Select outlined items={aircrafts}>Flight</Select>
+	<table class="mb-6">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Number</th>
+				<th>Gate <small>(Terminal - Number)</small></th>
+				<th>Equipment <small>(Carrier - Tail Number)</small></th>
+				<th>Scheduled Departure</th>
+				<th>Scheduled Arrival</th>
+				<th>Departure Status</th>
+				<th>#</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each $terminals as terminal, ind (terminal.id)}
+				<tr>
+					<td>{ind + 1}</td>
+					<td>{terminal.number}</td>
+					<td>{terminal.gateAssignment.terminal} - {terminal.gateAssignment.number}</td>
+					<td>{terminal.equipment.carrier} - {terminal.equipment.tailNumber}</td>
+					<td>{terminal.schedule.scheduledDeparture}</td>
+					<td>{terminal.schedule.scheduledArrival}</td>
+					<td>{terminal.schedule.departureStatus.actual}</td>
+					<td></td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 
-		<TextField outlined>Equipment - Terminal</TextField>
-		<TextField outlined>Equipment - Number</TextField>
+	<Button on:click={toggleDialog}>New Terminal</Button>
+	<Dialog persistent class="pa-8" bind:active={isDialogActive}>
+		<form on:submit|preventDefault={submit}>
+			<TextField outlined bind:value={formData.number}>Number</TextField>
 
-		<TextField outlined>Gate Assignment - Terminal</TextField>
-		<TextField outlined>Gate Assignment - Number</TextField>
+			<TextField outlined bind:value={formData.equipment.carrier}>Equipment - Carrier</TextField>
+			<TextField outlined bind:value={formData.equipment.tailNumber}>Equipment - Tail Number</TextField>
 
-		<TextField outlined>Scheduled Departure</TextField>
-		<TextField outlined>Scheduled Arrival</TextField>
+			<TextField outlined bind:value={formData.gateAssignment.terminal}>Gate Assignment - Terminal</TextField>
+			<TextField outlined bind:value={formData.gateAssignment.number}>Gate Assignment - Number</TextField>
 
-		<TextField outlined>Departure Status - Delayed By</TextField>
-		<TextField outlined>Departure Status - Actual</TextField>
-	</form>
+			<TextField outlined bind:value={formData.schedule.scheduledDeparture}>Scheduled Departure</TextField>
+			<TextField outlined bind:value={formData.schedule.scheduledArrival}>Scheduled Arrival</TextField>
+			<TextField outlined bind:value={formData.schedule.departureStatus.actual}>Departure Status - Actual</TextField>
+
+			<Row class="ml-0 mr-0">
+				<div style="flex:1; text-align: left;">
+					<Button class="success-color" type="submit">Create</Button>
+				</div>
+				<Button class="ml-3" type="reset">Reset</Button>
+				<Button class="error-color  ml-3" on:click={toggleDialog}>Cancel</Button>
+			</Row>
+		</form>
+	</Dialog>
 </CardForm>
 
