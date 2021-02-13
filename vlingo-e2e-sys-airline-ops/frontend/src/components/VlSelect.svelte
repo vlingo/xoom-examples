f0b472798b2880bf6aa0275f74392ea7cf529707
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-13 22:16:59 +0300

    air vlselect component is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ vlingo-e2e-sys-airline-ops/frontend/src/components/VlSelect.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Select } from 'svelte-materialify/src';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	export let value = "";
+
+  $: {
+    if (value) {
+      dispatch('change', value)
+    }
+  }
+</script>
+
+<Select { ...$$props } bind:value />
+
+<style>
+  :global(button) {
+    cursor: pointer;
+  }
+</style>
