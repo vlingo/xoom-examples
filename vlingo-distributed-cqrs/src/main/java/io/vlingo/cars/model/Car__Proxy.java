e9100bf629e34c4ca95115d27a1a5069c8724915
Petru Flueras <petru.flueras@gmail.com>
2021-04-01 21:37:50 +0300

    Introduction of kubernetes cluster deployment

---
+++ vlingo-distributed-cqrs/src/main/java/io/vlingo/cars/model/Car__Proxy.java
@@ -0,0 +1,40 @@
+package io.vlingo.cars.model;
+
+import io.vlingo.actors.*;
+import io.vlingo.actors.Definition.SerializationProxy;
+import io.vlingo.common.Completes;
+import io.vlingo.common.SerializableConsumer;
+
+public class Car__Proxy extends ActorProxyBase<io.vlingo.cars.model.Car> implements io.vlingo.cars.model.Car {
+
+  private static final String defineWithRepresentation1 = "defineWith(java.lang.String, java.lang.String, java.lang.String)";
+
+  private final Actor actor;
+  private final Mailbox mailbox;
+
+  public Car__Proxy(final Actor actor, final Mailbox mailbox){
+    super(io.vlingo.cars.model.Car.class, SerializationProxy.from(actor.definition()), actor.address());
+    this.actor = actor;
+    this.mailbox = mailbox;
+  }
+
+  public Car__Proxy(){
+    super();
+    this.actor = null;
+    this.mailbox = null;
+  }
+
+  public io.vlingo.common.Completes<io.vlingo.cars.model.CarState> defineWith(java.lang.String arg0, java.lang.String arg1, java.lang.String arg2) {
+    if (!actor.isStopped()) {
+      ActorProxyBase<Car> self = this;
+      final SerializableConsumer<Car> consumer = (actor) -> actor.defineWith(ActorProxyBase.thunk(self, (Actor)actor, arg0), ActorProxyBase.thunk(self, (Actor)actor, arg1), ActorProxyBase.thunk(self, (Actor)actor, arg2));
+      final io.vlingo.common.Completes<io.vlingo.cars.model.CarState> returnValue = Completes.using(actor.scheduler());
+      if (mailbox.isPreallocated()) { mailbox.send(actor, Car.class, consumer, Returns.value(returnValue), defineWithRepresentation1); }
+      else { mailbox.send(new LocalMessage<Car>(actor, Car.class, consumer, Returns.value(returnValue), defineWithRepresentation1)); }
+      return returnValue;
+    } else {
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, defineWithRepresentation1));
+    }
+    return null;
+  }
+}
