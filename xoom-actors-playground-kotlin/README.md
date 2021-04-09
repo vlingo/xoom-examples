006bae726efb962551f909be8b29fd3a90df0a9a
Jakub Zalas <jakub@zalas.pl>
2021-04-09 18:21:33 +0100

    [xoom-auth-authservice] Move io.vlingo to io.vlingo.xoom
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- xoom-actors-playground-kotlin/README.md
+++ xoom-actors-playground-kotlin/README.md
@@ -1,4 +1,4 @@
-# Kotlin Vlingo Actors
+# Kotlin Xoom Actors
 
 This project is one example of how to use the [xoom-Actors](https://github.com/vlingo).
 The idea is create a basic Kotlin Maven application to demostrate how works the vlingo platform
--- xoom-auth-authservice/src/main/java/io/vlingo/xoom/examples/authservice/infra/Bootstrap.java
+++ xoom-auth-authservice/src/main/java/io/vlingo/xoom/examples/authservice/infra/Bootstrap.java
@@ -4,10 +4,10 @@
 // Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
-package io.vlingo.authservice.infra;
+package io.vlingo.xoom.examples.authservice.infra;
 
-import io.vlingo.actors.World;
-import io.vlingo.http.resource.Server;
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.http.resource.Server;
 
 public class Bootstrap
 {
