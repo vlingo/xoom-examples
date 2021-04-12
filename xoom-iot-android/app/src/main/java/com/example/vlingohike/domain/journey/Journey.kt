7a02b635a8b86c55fa518219e55c1226cb748afe
Jakub Zalas <jakub@zalas.pl>
2021-04-12 12:49:55 +0100

    [xoom-iot-android] Remove an outdated example
    Latest revision the example is available on is 96668998825f086fa6b06bf606aafb6d606b08cf
    
    https://github.com/vlingo/vlingo-examples/tree/96668998825f086fa6b06bf606aafb6d606b08cf/xoom-iot-android

--- xoom-iot-android/app/src/main/java/com/example/vlingohike/domain/journey/Journey.kt
+++
@@ -1,7 +0,0 @@
-package com.example.vlingohike.domain.journey
-
-interface Journey {
-    fun step(step: Step)
-    fun inDanger()
-    fun safe()
-}
--- xoom-iot-android/app/src/main/java/com/example/vlingohike/domain/journey/Step.kt
+++
@@ -1,3 +0,0 @@
-package com.example.vlingohike.domain.journey
-
-data class Step(val altitude: Double, val longitude: Double, val latitude: Double)
