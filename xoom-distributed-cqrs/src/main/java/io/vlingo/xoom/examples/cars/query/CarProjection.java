e3a21857e21aa0066a01fa37d926ec4372edaf97
Petru Flueras <petru.flueras@gmail.com>
2021-04-28 00:34:57 +0300

    Several adjustments
    Signed-off-by: Petru Flueras <petru.flueras@gmail.com>

--- xoom-distributed-cqrs/src/main/java/io/vlingo/xoom/examples/cars/query/CarProjection.java
+++ xoom-distributed-cqrs/src/main/java/io/vlingo/xoom/examples/cars/query/CarProjection.java
@@ -68,13 +68,14 @@ public class CarProjection extends StateStoreProjectionActor<CarView> {
                 case CarRegistered:
                     CarEvents.CarRegistered registered = typed(event);
                     mergedData = mergedData.mergeWith(registered.carId, registered.registrationNumber);
+                    break;
                 case Unmatched:
                     logger().warn("Event of type " + event.typeName() + " was not matched.");
                     break;
             }
         }
 
-        logger().info("PROJECTED: " + mergedData);
+        logger().debug("PROJECTED: " + mergedData);
 
         return  mergedData;
     }
