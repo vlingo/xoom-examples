5735e35a4f7fe25598275816f62494066fa49e45
Petru Flueras <petru.flueras@gmail.com>
2022-07-23 13:12:53 +0300

    Adjustments based on latest changes of xoom-cluster (removed cluster nodes from configuration file)

--- xoom-distributed-ping-pong/src/main/java/io/vlingo/xoom/examples/pingpong/domain/Config.java
+++ xoom-distributed-ping-pong/src/main/java/io/vlingo/xoom/examples/pingpong/domain/Config.java
@@ -1,5 +1,15 @@
 package io.vlingo.xoom.examples.pingpong.domain;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public final class Config {
 	public static String nodeName = "undefined";
+
+	public static final Map<String, String> nodesConfig = Stream.of(new String[][] {
+					{"node1", "1:node1:true:localhost:37371:37372"},
+					{"node2", "2:node2:false:localhost:37373:37374"},
+					{"node3", "3:node3:false:localhost:37375:37376"}})
+					.collect(Collectors.toMap(entry -> entry[0], row -> row[1]));
 }
